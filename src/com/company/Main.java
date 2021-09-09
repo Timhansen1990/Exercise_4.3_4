79dbcfc1cd819e3846a82663e0751c10387b77d8
tim Hansen <timx1776@edu.easj.dk>
2021-09-09 10:45:18 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int result;
+
+        //System.out.println(result);//
+
+        // because I don't assign any value to result it cannot run this program.//
+
+        System.out.println("boo!" + 7);
+        // it will just print the value out as a full string because String is the default value of println.//
+    }
+}
